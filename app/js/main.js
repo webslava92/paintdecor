311ca03dee698d96b60e7c20b4d7d1ffa2ae6203
webslava92 <pustovit.slava@gmail.com>
2021-08-11 00:41:01 +0300

    add wrapper for cart update menu button

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-  
+  $('.header__menu-btn').on('click', function() {
+    $('.header__menu-btn').toggleClass('header__menu-btn--active');
+    $('.header__menu').toggleClass('header__menu--active');
+  });
 });

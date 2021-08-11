16ec5acb7c9db1007258e7c02febe3d01e564df2
webslava92 <pustovit.slava@gmail.com>
2021-08-12 00:21:17 +0300

    add footer section update header

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,13 @@ $(function(){
   $('.header__menu-btn').on('click', function() {
     $('.header__menu-btn').toggleClass('header__menu-btn--active');
     $('.header__menu').toggleClass('header__menu--active');
+    $('.header__product-menu').removeClass('header__product-menu--active');
+  });
+  $('.header__menu-list').on('click', function () {
+    $('.header__product-menu').toggleClass('header__product-menu--active');
+  });
+  $('.header__menu-link, .header__product-link').on('click', function () {
+    $('.header__product-menu').removeClass('header__product-menu--active');
+    $('.header__menu').removeClass('header__menu--active');
   });
 });
